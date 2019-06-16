9f311caf96f42fcc86ac487bcb8eded918d5828c
ambrizals <sabuncolek@ambrizal.net>
2019-06-16 15:00:19 +0800

    Update Tugas

---
+++ TUGAS4-MP-V-15101563_AS/app/src/main/java/com/ambrizals/tugas4/AlarmReceiver.java
@@ -0,0 +1,17 @@
+package com.ambrizals.tugas4;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.widget.Toast;
+
+public class AlarmReceiver extends BroadcastReceiver {
+    MediaPlayer player;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "Alarm Aktif !", Toast.LENGTH_LONG).show();
+        player = MediaPlayer.create(context, R.raw.alarm);
+    }
+}
